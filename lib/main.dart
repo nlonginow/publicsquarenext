5e848fb67d98539e3a734a15881a31b61de9980e
Nick Longinow <nicklonginow@gmail.com>
2023-04-01 14:30:14 -0400

    got start of a custom launcher icon, it is not done bc it needs to be made to be full screen

--- lib/main.dart
+++ lib/main.dart
@@ -38,22 +38,8 @@ void initializeFirebase() async {
 class MyApp extends StatelessWidget {
   MyApp({Key? key}) : super(key: key);
 
-  Map<int, Color> color = {
-    50: const Color.fromRGBO(250, 202, 88, .1),
-    100: const Color.fromRGBO(250, 202, 88, .2),
-    200: const Color.fromRGBO(250, 202, 88, .3),
-    300: const Color.fromRGBO(250, 202, 88, .4),
-    400: const Color.fromRGBO(250, 202, 88, .5),
-    500: const Color.fromRGBO(250, 202, 88, .6),
-    600: const Color.fromRGBO(250, 202, 88, .7),
-    700: const Color.fromRGBO(250, 202, 88, .8),
-    800: const Color.fromRGBO(250, 202, 88, .9),
-    900: const Color.fromRGBO(250, 202, 88, 1),
-  };
-
   @override
   Widget build(BuildContext context) {
-    final MaterialColor colorCustom = MaterialColor(0xFFFACA58, color);
     return MultiProvider(
         providers: [
           ChangeNotifierProvider.value(value: BottomNavigatorProvider())
